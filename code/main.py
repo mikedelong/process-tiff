eba82edfb4f63788c2ddc1671327951287c91549
mikedelong <mikedelong@outlook.com>
2018-08-17 09:58:51 -0400

    feature: first working version to produce 400 x 400 x 3 slices

--- code/main.py
+++ code/main.py
@@ -16,12 +16,26 @@ if __name__ == '__main__':
     console_handler.setLevel(logging.INFO)
     logger.info('started')
 
+    x_slice_size = 400
+    y_slice_size = 400
     input_folder = '../data/'
+    output_folder = '../output/'
     input_files = input_folder + '*.tif'
     for input_file in glob(input_files):
+        base_filename = input_file.replace('\\', '/').replace('.tif', '').replace(input_folder, '')
         image = Image.open(input_file)
         image_array = np.array(image)
         logger.info('%s %s' % (input_file, image_array.shape))
+        x_shape = image_array.shape[0]
+        y_shape = image_array.shape[1]
+        for x_index, x_start in enumerate(range(0, x_shape, x_slice_size)):
+            for y_index, y_start in enumerate(range(0, y_shape, y_slice_size)):
+                image_slice = image_array[x_start:x_start + x_slice_size, y_start:y_start + y_slice_size, 0:3]
+                if image_slice.shape == (x_slice_size, y_slice_size, 3):
+                    output_filename = '{}{}.{}.{}.tif'.format(output_folder, base_filename, x_index, y_index)
+                    logger.info('writing image section to %s' % output_filename)
+                    result = Image.fromarray(image_slice)
+                    result.save(output_filename)
 
     logger.info('done')
     finish_time = time()
